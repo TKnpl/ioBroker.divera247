abca81d85b7e5013d58f0e615bcda832cb25b087
TKnpl <dev@t-concepts.de>
2021-01-18 14:17:24 +0000

    adjusted translations

--- admin/words.js
+++ admin/words.js
@@ -2,28 +2,76 @@
 'use strict';
 
 systemDictionary = {
-	'divera247 adapter settings': {
-		'en': 'Adapter settings for divera247',
-		'de': 'Adaptereinstellungen für divera247'
+	"divera247 adapter settings": {
+		"en": "divera247 adapter settings",
+		"de": "divera247 Adaptereinstellungen",
+		"ru": "настройки адаптера divera247",
+		"pt": "configurações do adaptador divera247",
+		"nl": "divera247 adapter instellingen",
+		"fr": "Paramètres de l'adaptateur divera247",
+		"it": "impostazioni dell'adattatore divera247",
+		"es": "configuración del adaptador divera247",
+		"pl": "Ustawienia adaptera divera247",
+		"zh-cn": "divera247适配器设置"
+  	},
+	"Access-Key": {
+	    "en": "Access-Key",
+	    "de": "Zugangs-Key",
+	    "ru": "Ключ доступа",
+	    "pt": "Chave de acesso",
+	    "nl": "Toegangssleutel",
+	    "fr": "Clé d'accès",
+	    "it": "Chiave di accesso",
+	    "es": "Llave de acceso",
+	    "pl": "Klucz dostępu",
+	    "zh-cn": "存取键"
 	},
-	'diveraAccessKey': {
-		'en': 'Access-Token',
-		'de': 'Zugangs-Key'
-	},
-	'diveraAccessKeyInformation': {
-		'en': 'personal access token for Divera 24/7',
-		'de': 'Persönlicher Zugangs-Key für Divera 24 / 7'
-	},
-	'diveraConfigurationHelpLink': {
-		'en': 'Configuration information',
-		'de': 'Einrichtungshilfe'
-	},
-	'pollIntervall': {
-		'en': 'Poll intervall (seconds)',
-		'de': 'Aktualisierungsintervall (Sekunden)'
-	},
-	'pollIntervallInformation': {
-		'en': 'At least 10 seconds',
-		'de': 'Mindestens 10 Sekunden'
-	}
+	"Personal Access-Key for Divera 24 / 7": {
+	    "en": "Personal Access-Key for Divera 24 / 7",
+	    "de": "Persönlicher Zugangs-Key für Divera 24/7",
+	    "ru": "Персональный ключ доступа для Divera 24/7",
+	    "pt": "Chave de acesso pessoal para Divera 24/7",
+	    "nl": "Persoonlijke toegangssleutel voor Divera 24/7",
+	    "fr": "Clé d'accès personnelle pour Divera 24/7",
+	    "it": "Chiave di accesso personale per Divera 24/7",
+	    "es": "Clave de acceso personal para Divera 24/7",
+	    "pl": "Osobisty klucz dostępu do Divera 24/7",
+	    "zh-cn": "Divera 24/7的个人访问密钥"
+  	},
+	"Configuration help": {
+	    "en": "Configuration help",
+	    "de": "Konfigurationshilfe",
+	    "ru": "Помощь в настройке",
+	    "pt": "Ajuda de configuração",
+	    "nl": "Configuratiehulp",
+	    "fr": "Aide à la configuration",
+	    "it": "Guida alla configurazione",
+	    "es": "Ayuda de configuración",
+	    "pl": "Pomoc w konfiguracji",
+	    "zh-cn": "配置帮助"
+  	},
+	"update interval (seconds)": {
+	    "en": "update interval (seconds)",
+	    "de": "Aktualisierungsintervall (Sekunden)",
+	    "ru": "интервал обновления (секунды)",
+	    "pt": "intervalo de atualização (segundos)",
+	    "nl": "update-interval (seconden)",
+	    "fr": "intervalle de mise à jour (secondes)",
+	    "it": "intervallo di aggiornamento (secondi)",
+	    "es": "intervalo de actualización (segundos)",
+	    "pl": "interwał aktualizacji (sekundy)",
+	    "zh-cn": "更新间隔（秒）"
+  	},
+	"minimum 10 seconds": {
+	    "en": "minimum 10 seconds",
+	    "de": "mindestens 10 Sekunden",
+	    "ru": "минимум 10 секунд",
+	    "pt": "mínimo 10 segundos",
+	    "nl": "minimaal 10 seconden",
+	    "fr": "minimum 10 secondes",
+	    "it": "minimo 10 secondi",
+	    "es": "mínimo 10 segundos",
+	    "pl": "minimum 10 sekund",
+	    "zh-cn": "最少10秒"
+  	}
 };
